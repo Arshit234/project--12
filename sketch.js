a554027279af65a8180bf97f5e6d7072871d015a
Arshit234 <105442468+Arshit234@users.noreply.github.com>
2022-05-20 22:13:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function preload(){
+  //pre-load images
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+  
+}
+
+function draw() {
+  background(0);
+
+
+
+
+
+  drawSprites()
+}
